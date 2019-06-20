1c7723fb3c5504c896fb47bdf817d7e085a87c0c
devapriyabandara <devapriyabandara@gmail.com>
2019-06-21 00:42:20 +1000

    Initia commit of spring boot project

---
+++ src/test/java/org/devapriya/PlantPlaces/PlantPlacesApplicationTests.java
@@ -0,0 +1,16 @@
+package org.devapriya.PlantPlaces;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PlantPlacesApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
